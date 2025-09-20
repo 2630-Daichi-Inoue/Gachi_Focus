2093ceb16d30b0086aba88b6463fb3ef89265880
中塚星来 <>
2025-09-20 18:33:28 +0900

    resister page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/room-b', [ReservationController::class, 'roomB']);
+Route::post('/room-b', [ReservationController::class, 'reserve']);
+
+Route::resource('reservations', ReservationController::class)
+     ->only(['show','edit','update'])
+     ->middleware('auth');
+
+
+
+     
+Route::get('/reserve', [ReservationController::class, 'show'])   ->name('reserve.coworkingspace');
+Route::get('/rooms/{slug}', [ReservationController::class, 'show'])   ->name('rooms.show');
+Route::post('/rooms/{slug}/reserve', [ReservationController::class, 'store'])->name('rooms.reserve');
+
+
