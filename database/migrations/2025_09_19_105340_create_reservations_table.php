fc2d9be9f97765705039a1616ae226691a9c3b66
Daichi <inouedaichi777@gmail.com>
2025-10-21 19:39:28 +0900

    no message

--- database/migrations/2025_09_19_105340_create_reservations_table.php
+++ database/migrations/2025_09_19_105340_create_reservations_table.php
@@ -44,7 +44,7 @@ return new class extends Migration
 
     }
 
-// INSERT INTO `reservations`(`id`, `user_id`, `space_id`, `rating`, `comment`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 1, 1, 5, 'Great!', '2025-01-01 00:00:00', null, null), (2, 2, 1, 4, 'Nice!', '2025-01-01 00:00:00', null, null), (3, 2, 1, 4, 'Good!', '2025-01-01 00:00:00', null, null), (4, 1, 1, 5, 'Wonderful', '2025-01-01 00:00:00', null, null);
+// INSERT INTO `reservations`(`id`, `user_id`, `room`, `type`, `date`, `start_time`, `end_time`, `adults`, `facilities`, `total_price`, `created_at`, `updated_at`, `deleted_at`, `space_id`) VALUES (1, 1, 'A', 'Focus Booth', '2025-04-01', '08:00:00', '12:00:00', 10, null, 2400, '2025-01-01 00:00:00', null, null, 1);
 
     /**
      * Reverse the migrations.
