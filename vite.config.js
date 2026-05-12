1437dbf60177454f746327129feae22493049c04
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2026-05-13 07:35:09 +0900

    fix: add admin assets to Vite input

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/js/app.js',
+                'resources/js/admin.js',
+                'resources/sass/app.scss',
+                'resources/css/admin.css',
+            ],
             refresh: true,
         }),
         vue({
